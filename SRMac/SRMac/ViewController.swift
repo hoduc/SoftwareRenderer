54d678cac4e9ba6689a9aa9f6ef4669227b59acd
hoduc <duc_ho90@yahoo.com>
2017-07-26 16:38:48 -0700

    render imageview from char array

--- SRMac/SRMac/ViewController.swift
+++ SRMac/SRMac/ViewController.swift
@@ -10,19 +10,11 @@ import Cocoa
 
 class ViewController: NSViewController {
     
+    @IBOutlet weak var imageView: NSImageView! //to display rendered image
     var appBridge: Application_Bridge!;
     //static var countload = 0
     override func viewDidLoad() {// DOES NOT MEAN APPDELEGATE ACTIVE YET
         super.viewDidLoad()
-//        if appBridge != nil {
-//            return;
-//        }
-//        appBridge = Application_Bridge();
-//        //initialize application
-//        appBridge.appInitialize();
-//        //ViewController.countload += 1;
-//        print(ViewController.countload);
-        // Do any additional setup after loading the view.
     }
 
     override var representedObject: Any? {
@@ -37,8 +29,49 @@ class ViewController: NSViewController {
         appBridge.appInitialize();
     }
     
+    func imgFromBitmap(frameBuffer: UnsafeMutablePointer<Int8>!, width: Int, height: Int) -> NSImage?{
+        var frameBuffer = frameBuffer;
+        let pixelDataSize = MemoryLayout<Int8>.size;
+//        let data: Data = withUnsafePointer(to: &frameBuffer){
+//            Data(bytes: UnsafePointer($0), count: pixelDataSize);
+//        };
+        let data = Data(buffer: UnsafeBufferPointer(start: &frameBuffer, count: width*height));
+        let cfdata = NSData(data: data) as CFData
+        let provider: CGDataProvider! = CGDataProvider(data: cfdata)
+        
+        if provider == nil {
+            print("CGDataProvider is not supposed to be nil");
+            return nil
+        }
+        let cgimage: CGImage! = CGImage(
+            width: width,
+            height: height,
+            bitsPerComponent: 8,
+            bitsPerPixel: 24,
+            bytesPerRow: width * pixelDataSize,
+            space: CGColorSpaceCreateDeviceRGB(),
+            bitmapInfo: CGBitmapInfo(rawValue: CGImageAlphaInfo.premultipliedFirst.rawValue),
+            provider: provider,
+            decode: nil,
+            shouldInterpolate: false,
+            intent: .defaultIntent
+        )
+        
+        if cgimage == nil {
+            print("CGImage is not supposed to be nil");
+            return nil;
+        }
+        
+        return NSImage(cgImage: cgimage, size: NSSize(width: width, height: height));
+    }
+    
     func Render(){
         appBridge.render();
+        //var fb = appBridge.getFrameBuffer();
+        
+        imageView.image = imgFromBitmap(frameBuffer: appBridge.getFrameBuffer(), width: Int(appBridge.getFrameBufferWidth()), height: Int(appBridge.getFrameBufferHeight()));
+        
+        
     }
 
 
